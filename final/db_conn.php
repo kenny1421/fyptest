8ea512ea887b64ebe1fd7fdcd0a3f22794904fab
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-11 14:10:48 +0800

    Update db_conn.php

--- final/db_conn.php
+++ final/db_conn.php
@@ -4,15 +4,18 @@
 $sname = "mysqltest3.mysql.database.azure.com";
 $uname = "sqltest";
 $password = "Test12345";
-
 $db_name = "my_db";
 
+//Initializes MySQLi
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+//Establish the connection
+mysqli_real_connect($conn, 'mysqltest3.mysql.database.azure.com','sqltest', 'Test12345', 'my_db', 3306, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$conn) {
-	echo "HEllo1 world";
+	
 	echo "Connection Failed!";
-	echo ("Error msg " . mysqli_connect_errno());
 	exit();
 }
