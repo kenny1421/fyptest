94354e7844052e2d5654bf3f30c4fe3d84c6590b
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-11 13:47:58 +0800

    Update create.php

--- final/php/create.php
+++ final/php/create.php
@@ -10,16 +10,17 @@ if (isset($_POST['create'])){
     $username = validate($_POST['username']);
     $password = validate($_POST['password']);
     $role = validate($_POST['role']);
+    $NAME = validate($_POST['NAME']);
     $hash = md5($password);
     
-    $user_data ='username='.$username. '&password='.$password. '&role='.$role; 
+    $user_data ='username='.$username. '&password='.$password. '&role='.$role. '&NAME='.$NAME; 
     if (empty($username)){
         header("Location: ../CreateAccount.php?error=username is required&$user_data");
     }else if(empty($password)){
         header("Location: ../CreateAccount.php?error=password is required&$user_data");
     }else{
-        $sql = "INSERT INTO users(username,password,role)
-        VALUES('$username', '$hash', '$role')";
+        $sql = "INSERT INTO users(username,password,role,NAME)
+        VALUES('$username', '$hash', '$role', '$NAME')";
         $result = mysqli_query($conn, $sql);
         if ($result) {
             header('Location: ../CreateAccount.php');
