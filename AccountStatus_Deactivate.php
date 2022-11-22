42d926b6038feee67ca784fd08a56b2ce7d84727
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-22 15:12:36 +0800

    Delete AccountStatus_Deactivate.php

--- AccountStatus_Deactivate.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $id=$_GET['id'];
-    include 'db_conn.php';
-    mysqli_query($conn, "UPDATE users SET status='Deactivate' WHERE id='$id'");
-    header("location:Admin_ManageAccount.php")
-
-?>
