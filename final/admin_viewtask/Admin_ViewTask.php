94c0057f6ba250f4393c0affcd6256eab354fd36
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-25 04:46:54 +0800

    Update Admin_ViewTask.php

--- final/admin_viewtask/Admin_ViewTask.php
+++ final/admin_viewtask/Admin_ViewTask.php
@@ -150,7 +150,7 @@ $blobList = $blobClient->listBlobs($containerName, $listBlobsOptions);
                                     $blob->getName()?>">Download</a>
                                 </td>
                                 <td>
-                                    <a href=" ../final/user_upload/User_UploadTask.php?delete_id=<?php echo
+                                    <a href=" ../user_upload/User_UploadTask.php?delete_id=<?php echo
                                     $blob->getName()?>">Delete</a>
                                 </td>
                             </tr>
