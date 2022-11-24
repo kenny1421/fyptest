dc62356b7378346860d0045eba8a743670cebe6b
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-25 02:46:13 +0800

    Add files via upload

---
+++ final/admin_viewtask/index.php
@@ -0,0 +1,38 @@
+<?php include 'filesLogic.php';?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>Files Upload and Download</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class ="row">
+                <table>
+                    <thead>
+                        <th>ID</th>
+                        <th>FileName</th>
+                        <th>Size (in mb)</th>
+                        <th>Downloads</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach($files as $file): ?>
+                        
+                        <tr>
+                            <td><?php echo $file['id'];?></td>
+                            <td><?php echo $file['name'];?></td>
+                            <td><?php echo $file['size']/1000 . "KB";?></td>
+                            <td><?php echo $file['downloads'];?></td>
+                            <td>
+                                <a href="index.php?file_id=<?php echo
+                                    $file['id']?>">Download</a>
+                            </td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+            </table>
+        </div>
+    </div>
+  </body>
+</html>
