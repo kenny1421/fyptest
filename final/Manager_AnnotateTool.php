91caa791c357b1c7a4ebbc5fa8cc4eb42ba7ac78
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-25 06:44:39 +0800

    Update Manager_AnnotateTool.php

--- final/Manager_AnnotateTool.php
+++ final/Manager_AnnotateTool.php
@@ -30,10 +30,10 @@
     <li>
 		<a href="Manager_ToDoList.php">
 			<i class='bx bx-task' ></i>
-				<span class="link_name">To Do List</span>
+				<span class="link_name">Todo List</span>
 					</a>
 					<ul class="sub-menu blank">
-						<li><a class="link_name" href="Manager_ToDoList.php">To Do List</a></li>
+						<li><a class="link_name" href="Manager_ToDoList.php">Todo List</a></li>
 					</ul>
 	</li>
       <li>
