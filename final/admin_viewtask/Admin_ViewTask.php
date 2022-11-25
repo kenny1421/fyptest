80bfc6d0530c9b612d257f7c92c150069a62676d
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-25 08:45:29 +0800

    Update Admin_ViewTask.php

--- final/admin_viewtask/Admin_ViewTask.php
+++ final/admin_viewtask/Admin_ViewTask.php
@@ -153,7 +153,7 @@ $blobList = $blobClient->listBlobs($containerName, $listBlobsOptions);
                                 <td><?php echo $blob->getName();?></td>
                                 <td>
                                     <a href="Admin_ViewTask.php?file_id=<?php echo
-                                    $blob->getName()?>"><button class="btn btn-primary"Download</button></a>
+                                    $blob->getName()?>"><button class="btn btn-primary">Download</button></a>
                                 </td>
                             </tr>
                         <?php
