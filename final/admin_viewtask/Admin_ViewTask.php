c59f51c2864b737ac4824c9b6890fb510094f2c6
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-25 08:40:49 +0800

    Update Admin_ViewTask.php

--- final/admin_viewtask/Admin_ViewTask.php
+++ final/admin_viewtask/Admin_ViewTask.php
@@ -136,12 +136,12 @@ $blobList = $blobClient->listBlobs($containerName, $listBlobsOptions);
         </form>
       </div>
         <div class ="row">
-                <table>
+                <table class="table">
                     <thead>
-                        <th>ID</th>
-                        <th>FileName</th>
-                        <th>Action</th>
-                        <th></th>
+                        <th scope="col">ID</th>
+                        <th scope="col">FileName</th>
+                        <th scope="col">Action</th>
+                        
                     </thead>
                     <tbody>
                         <?php
@@ -153,7 +153,7 @@ $blobList = $blobClient->listBlobs($containerName, $listBlobsOptions);
                                 <td><?php echo $blob->getName();?></td>
                                 <td>
                                     <a href="Admin_ViewTask.php?file_id=<?php echo
-                                    $blob->getName()?>">Download</a>
+                                    $blob->getName()?>"><button class="btn btn-primary"Download</button></a>
                                 </td>
                             </tr>
                         <?php
