2e4f52412ecbe0e52876df2c100c58f48ccc7d00
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-25 08:32:22 +0800

    Update Admin_AddTask.php

--- final/Admin_AddTask.php
+++ final/Admin_AddTask.php
@@ -142,27 +142,26 @@ if(isset($_POST['submit'])){
             <div class="container my-5">
 
               <form method="post">
-                  <div class="form-group">
-                      <label>Task Description</label>
+                  <div class="mb-3">
+                      <label for="description" class="form-label">Task Description</label>
                       <input type="text" class="form-control" placeholder="Enter task description" name="taskdescription" autocomplete="off">
                   </div>
-		      <br>
-                  <div class="form-group">
-                      <label>Annotators Name</label>
+                  
+                  <div class="mb-3">
+                      <label for="name" class="form-label">Annotators Name</label>
                       <input type="text" class="form-control" placeholder="Enter annotators name" name="name" autocomplete="off">
                   </div>
-		      <br>
-                  <div class="form-group">
-                      <label>Email</label>
+                  
+                  <div class="mb-3">
+                      <label for="email" class="form-label">Email</label>
                       <input type="email" class="form-control" placeholder="Enter annotators email" name="email" autocomplete="off">
                   </div>
-		      <br>
-                  <div class="form-group">
-                      <label>Task Comment</label>
+                  
+                  <div class="mb-3">
+                      <label for="comment" class="form-label">Task Comment</label>
                       <input type="text" class="form-control" placeholder="Enter task comment" name="taskcomment" autocomplete="off">
                   </div>
-		      <br>
-      
+                  <br>      
       
       
       
