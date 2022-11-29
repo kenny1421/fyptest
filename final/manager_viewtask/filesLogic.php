bd8f53df3314556c3e4f67f3226f110fec49fded
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-30 00:17:48 +0800

    Update filesLogic.php

--- final/manager_viewtask/filesLogic.php
+++ final/manager_viewtask/filesLogic.php
@@ -2,6 +2,7 @@
 // connect to the database
 $conn = mysqli_connect('mysqltest3.mysql.database.azure.com', 'sqltest', 'Test12345', 'my_db');
 
+
 $sql = "SELECT * FROM files";
 $result = mysqli_query($conn, $sql);
 
@@ -25,10 +26,49 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
     
 // Uploads files
       
+if (isset($_POST['save'])) { // if save button on the form is clicked
 
+        // Create container.
    
-}
+    // name of the uploaded file
+    // destination of the file on the server
+    
+        
+       $filename = $_FILES['myfile']['name'];
+        
+    if(($_FILES['myfile']['name']) == ""){
+        echo "<div class='text-danger text-center'>Please choose a file!</div>";
+    } else {
 
+        
+    $destination = '../uploads/' . $filename;
+
+    // get the file extension
+    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+    $file = $_FILES['myfile']['tmp_name'];
+    $size = $_FILES['myfile']['size'];
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+        
+    fclose($myfile);
+    $content = fopen($_FILES['myfile']["tmp_name"], "r")or die("Unable to open file!");
+       
+
+    if (!in_array($extension, ['zip', 'pdf', 'docx','json'])) {
+        echo "<div class='text-danger text-center'>You file extension must be .zip, .pdf ,.docx or json!!!</div>";
+    } elseif ($_FILES['myfile']['size'] > 99999999) { // file shouldn't be larger than 1Megabyte
+        echo "<div class='text-danger text-center'>File too large!</div>";
+    } else 
+     //Upload blob
+        
+    {
+        $blobClient->createBlockBlob($containerName, $filename, $content);
+        echo "<div class='text-success text-center'>" . $filename . " had been uploaded!</div>";
+        
+        }
+    }
+
+    }
+}
 if (isset($_GET['delete_id'])) {
     $blobfile = $_GET['delete_id'];
     
@@ -40,52 +80,33 @@ if (isset($_GET['delete_id'])) {
 
 // Downloads files
 if (isset($_GET['file_id'])) {
-    
-
     // fetch file to download from database
     $blobfile = $_GET['file_id'];
-    echo $blobfile;
-    echo "<div class='text-success text-center'>" . $blobfile . " had been downloaded!</div>";
-    $id = $_GET['file_id'];
     $filedoc = basename($blobfile);
     $ext = new SplFileInfo($filedoc);
     $fileext = strtolower($ext->getExtension());
 
-
-    /*if (file_exists($filepath)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename=' . basename($filepath));
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize('../uploads/' . $file['name']));
+    try {
+    $blob = $blobClient->getBlob('fileupload2',  $blobfile);
         
-        //This part of code prevents files from being corrupted after download
-        ob_clean();
-        flush();*/
-        try {
-    // Get blob.
-    $blob = $blobClient->getBlob('fileupload2', $blobfile);
-
     if($fileext === "pdf") {
         header('Content-type: application/pdf');
-    } else if ($fileext === "doc") {
-        header('Content-type: application/msword'); 
-    } else if ($fileext === "docx") {
+    } else if ($fileext == "zip") {
+        header('Content-type: application/zip'); 
+    } else if ($fileext == "docx") {
         header('Content-type: application/vnd.openxmlformats-officedocument.wordprocessingml.document'); 
-    } else if($fileext === "txt") {
+    } else if($fileext == "txt") {
         header('Content-type: plain/text');
-    }else if($fileext ==="json"){
+    }else if($fileext =="json"){
         header('Content-type:application/json');
+    } elseif($fileext == "jpg"){
+        header('Content-type:image/jpg');
     }
-            elseif($fileext === "jpg"){
-        header('Context-type:image/jpg');
-    }
-    header("Content-Disposition: attachment; filename=\"" . $blobfile . "\"");
-    
+    header("Content-Disposition: attachment; filename=\"$blobfile\"");   
     fpassthru($blob->getContentStream());
-    
+    echo "<div class='text-success text-center'>" . $blobfile . " had been downloaded!</div>";
+   
+        
 }
 catch(ServiceException $e){
     // Handle exception based on error codes and messages.
@@ -95,12 +116,5 @@ catch(ServiceException $e){
     $error_message = $e->getMessage();
     echo $code.": ".$error_message."<br />";
 }
-        readfile('../uploads/' . $file['name']);
 
-        // Now update downloads count
-        $newCount = $file['downloads'] + 1;
-        $updateQuery = "UPDATE files SET downloads=$newCount WHERE id=$id";
-        mysqli_query($conn, $updateQuery);
-        exit;
 }
-
