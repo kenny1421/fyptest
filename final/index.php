75f3fc8133c2f61bd1f965ba1cda115a76a22113
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-30 01:08:24 +0800

    Update index.php

--- final/index.php
+++ final/index.php
@@ -5,7 +5,7 @@
 <html>
 <head>
 	<title>NeounAI</title>
-	<link rel="icon" type="image/png" href="../Images/NeuonAI_Logo.png"/>
+	<link rel="icon" type="image/png" href="Images/NeuonAI_Logo.png"/>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
