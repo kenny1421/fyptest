4c292e80822780c6366124d542bb80c779d59474
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-30 01:03:01 +0800

    Update User_ViewTask.php

--- final/user_viewtask/User_ViewTask.php
+++ final/user_viewtask/User_ViewTask.php
@@ -28,7 +28,7 @@ $blobList = $blobClient->listBlobs($containerName, $listBlobsOptions);
     <link rel="icon" type="image/png" href="../Images/NeuonAI_Logo.png"/>
     <!--<title> Drop Down Sidebar Menu | CodingLab </title>-->
     <link rel="stylesheet" href="../css/style.css">
-    <!-- Boxiocns CDN Link -->
+    <!-- .Boxiocns CDN Link. -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
