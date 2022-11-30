c9414b86e1afd2cb0e50990c1365baee8173ad67
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-30 15:46:44 +0800

    Update index.php

--- final/index.php
+++ final/index.php
@@ -45,7 +45,7 @@
 		  <div class="mb-3 form-check">
 		    <input type="checkbox"
 			   class="form-check-input"
-			   id="exampleCheck1"
+			   id="showPassCheckBox"
 			   onclick="myFunction()">
 		    <label class="form-check-label" for="flexCheckDefault">
 			    Show Password
