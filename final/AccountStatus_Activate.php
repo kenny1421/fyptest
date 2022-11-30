9e3d4b0e2d7c85244a86812db861bac1b5e21ac5
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-30 17:01:08 +0800

    Update AccountStatus_Activate.php

--- final/AccountStatus_Activate.php
+++ final/AccountStatus_Activate.php
@@ -1,7 +1,7 @@
 <?php
      $id=$_GET['id'];
     include 'db_conn.php';
-    mysqli_query($conn, "UPDATE users SET status='Activate' WHERE id='$id'");
+    mysqli_query($conn, "UPDATE users SET status='Active' WHERE id='$id'");
     header("location:Admin_ManageAccount.php")
 
 ?>
