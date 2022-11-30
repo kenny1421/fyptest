a50f8fba04f10081069903ae0e296b38c15b47b6
kenny1421 <66106413+kenny1421@users.noreply.github.com>
2022-11-30 16:04:15 +0800

    Update Admin_UpdateTask.php

--- final/Admin_UpdateTask.php
+++ final/Admin_UpdateTask.php
@@ -173,8 +173,10 @@ if(isset($_POST['submit'])){
 		  <br>
 
 
-
-            <button type="submit" class="btn btn-primary" name="submit">Update</button>
+	    <div class="d-grid gap-2 d-md-block">
+	    	<button type="button" class="btn btn-primary" name="submit">Update</button>
+		<a href="Admin_ManageTask.php"><button class="btn btn-danger">Cancel</button></a>
+	    </div>
         </form>
 
 </div>
